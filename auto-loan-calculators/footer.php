3f3804c3f4b04eda0eb6653b5615fc8b479b4e35
Carlo Piantini <carlo.piantini@gmail.com>
2020-03-17 12:52:27 -0400

    Updates to the header and footer templates of the site theme

--- auto-loan-calculators/footer.php
+++ auto-loan-calculators/footer.php
@@ -10,22 +10,12 @@
  */
 
 ?>
-
+		</div><!--.inner-->
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'auto-loan-calculators' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'auto-loan-calculators' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'auto-loan-calculators' ), 'auto-loan-calculators', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+		<div class="site-info inner">
+			&#169; 2020 - Auto Loan Calculators - Developed by Carlo Piantini
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
--- auto-loan-calculators/header.php
+++ auto-loan-calculators/header.php
@@ -13,7 +13,7 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -21,38 +21,49 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'auto-loan-calculators' ); ?></a>
+<?php if (is_user_logged_in()) : ?>
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', 'auto-loan-calculators'); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$auto_loan_calculators_description = get_bloginfo( 'description', 'display' );
-			if ( $auto_loan_calculators_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $auto_loan_calculators_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+		<header id="masthead" class="site-header">
+			<div id="top-navigation-bar">
+				<div class="inner">
+					<a id="site-logo" href="<?php echo bloginfo('url'); ?>">Your Site Logo - AutoLoanCalculators.com</a><!--#site-logo-->
+					<nav id="top-navigation">
+						<?php wp_nav_menu(array(
+							'theme_location' => 'top',
+							'menu_id'        => 'top-menu',
+							'menu_class'	 => 'site-menu',
+							'container'		 => false
+						));
+						?>
+					</nav><!--#top-navigation-->
+				</div><!--.inner-->
+			</div><!--#top-navigation-bar-->
+			
+			<div id="site-navigation-bar">
+				<div class="inner">
+					<nav id="site-navigation">
+						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'auto-loan-calculators' ); ?></button>
+						<?php wp_nav_menu( array(
+							'theme_location' => 'primary',
+							'menu_id'        => 'primary-menu',
+							'menu_class'	 => 'site-menu',
+							'container'		 => false
+						) );
+						?>
+					</nav><!-- #site-navigation -->
+					<a id="new-price-cta" href="#">New Car Price Quote</a><!--#new-price-cta-->
+				</div><!--.inner-->
+			</div><!--#site-navigation-bar-->
+		</header><!-- #masthead -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'auto-loan-calculators' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+		<div id="content" class="site-content">
+			<div class="inner">
+<?php else : ?>
+	<div id="page" class="site coming-soon">
+		<h1 id="coming-soon-header">A New WordPress Site</h1><!--#coming-soon-header-->
+		<h2 id="coming-soon-subheader">Coming Soon!</h2><!--#coming-soon-subheader-->
+		<a id="coming-soon-login" href="/wp-admin">Admin Login</a><!--#coming-soon-login-->
+	</div>
+<?php endif; ?>
